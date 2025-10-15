775a90331089ef25a4548abd7e8538c862827066
josepereza <josepereza66@gmail.com>
2025-10-15 19:52:19 +0200

    first commit13

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   // Habilita la validación global de DTOs usando class-validator
   app.useGlobalPipes(
     new ValidationPipe({
